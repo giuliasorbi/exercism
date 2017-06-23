8b59b45449573d4688ad665ce59627e4edb56048
giuliasorbi <giulia.sorbi.91@gmail.com>
2017-06-23 15:02:53 +0200

    Grade School - add sorted names

--- cpp/grade-school/grade_school.cpp
+++ cpp/grade-school/grade_school.cpp
@@ -11,27 +11,21 @@ school::school() {
 }
 
 std::map<int, std::vector<std::string>> school::roster() const {
-    std::map<int, std::vector<std::string>> result = m_roster;
-    for (auto &grade_names : result) {
-        std::sort( grade_names.second.begin(), grade_names.second.end() ); 
-    }
-    return result;
+    return m_roster;
 }
 
 void school::add( std::string name, int grade ) {
-    m_roster[grade].push_back(name);
+    std::vector<std::string>::iterator it = std::upper_bound( m_roster[grade].begin(), m_roster[grade].end(), name ); 
+    m_roster[grade].insert( it, name );
 }
 
 std::vector<std::string> school::grade(int g) const {
-    std::vector <std::string> result;
     try {
-        result = m_roster.at(g);
-        std::sort(result.begin(), result.end() );
+        return m_roster.at(g);
     }
     catch( std::out_of_range e ) {
-        result = {};
+        return {};
     }
-    return result;
 }
 
 }
